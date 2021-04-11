38d34bcde6c97c90f0a56132e0c203701e8c1d33
zhangshixing-chn <1129836649@qq.com>
2021-04-11 20:34:15 +0800

    add pie file

---
+++ Pie/Customized_pie.py
@@ -0,0 +1,35 @@
+import pyecharts.options as opts
+from pyecharts.charts import Pie
+
+x_data = ["直接访问", "邮件营销", "联盟广告", "视频广告", "搜索引擎"]
+y_data = [335, 310, 274, 235, 400]
+data_pair = [list(z) for z in zip(x_data, y_data)]
+data_pair.sort(key=lambda x: x[1])
+
+(
+    Pie(init_opts=opts.InitOpts(width="1600px", height="800px", bg_color="#2c343c"))
+    .add(
+        series_name="访问来源",
+        data_pair=data_pair,
+        rosetype="radius",
+        radius="55%",
+        center=["50%", "50%"],
+        label_opts=opts.LabelOpts(is_show=False, position="center"),
+    )
+    .set_global_opts(
+        title_opts=opts.TitleOpts(
+            title="Customized Pie",
+            pos_left="center",
+            pos_top="20",
+            title_textstyle_opts=opts.TextStyleOpts(color="#fff"),
+        ),
+        legend_opts=opts.LegendOpts(is_show=False),
+    )
+    .set_series_opts(
+        tooltip_opts=opts.TooltipOpts(
+            trigger="item", formatter="{a} <br/>{b}: {c} ({d}%)"
+        ),
+        label_opts=opts.LabelOpts(color="rgba(255, 255, 255, 0.3)"),
+    )
+    .render("Customized_pie.html")
+)
---
+++ Pie/Doughnut_chart.py
@@ -0,0 +1,23 @@
+import pyecharts.options as opts
+from pyecharts.charts import Pie
+
+x_data = ["直接访问", "邮件营销", "联盟广告", "视频广告", "搜索引擎"]
+y_data = [335, 310, 234, 135, 1548]
+
+(
+    Pie(init_opts=opts.InitOpts(width="1600px", height="1000px"))
+    .add(
+        series_name="访问来源",
+        data_pair=[list(z) for z in zip(x_data, y_data)],
+        radius=["50%", "70%"],
+        label_opts=opts.LabelOpts(is_show=False, position="center"),
+    )
+    .set_global_opts(legend_opts=opts.LegendOpts(pos_left="legft", orient="vertical"))
+    .set_series_opts(
+        tooltip_opts=opts.TooltipOpts(
+            trigger="item", formatter="{a} <br/>{b}: {c} ({d}%)"
+        ),
+        # label_opts=opts.LabelOpts(formatter="{b}: {c}")
+    )
+    .render("Doughnut_chart.html")
+)
